92e5b66878f0866ca031eec89dede787542e452b
jack bindin <jackbindin95@hotmail.co.uk>
2022-07-16 13:55:35 +0200

    renaming method

--- src/main/java/com/example/demo/service/SatelliteService.java
+++ src/main/java/com/example/demo/service/SatelliteService.java
@@ -3,31 +3,38 @@ package com.example.demo.service;
 import com.example.demo.model.SatelliteLocation;
 import com.example.demo.repository.IssRepository;
 import com.example.demo.webclient.IssWebClientInterface;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.List;
 
+import static java.rmi.server.LogStream.log;
+
 @Service
+@Log4j2
 public class SatelliteService implements SatelliteServiceInterface{
+    private final int TABLE_CLEANUP_DELAY = 5 * 60 * 1000;
+    private final int ISS_ID = 25544;
+    private final int MAX_TABLE_SIZE = 10;
     @Autowired
     IssRepository issRepository;
     @Autowired
     IssWebClientInterface issWebClient;
-
-    private final int TABLE_CLEANUP_DELAY = 5 * 60 * 1000;
-    private final int ISS_ID = 25544;
-    private final int MAX_TABLE_SIZE = 10;
 
     @Override
     @Scheduled(fixedDelay = TABLE_CLEANUP_DELAY)
     public void refreshStoredLocation() {
         SatelliteLocation satelliteLocation = issWebClient.satelliteById(ISS_ID).block();
         issRepository.save(satelliteLocation);
+        log.info("New iss location saved at :"+ LocalDateTime.now());
         while(issRepository.findAll().size()>MAX_TABLE_SIZE){
             issRepository.delete(issRepository.findAll().get(0));
+            log.info("Reached max storage of locations ("+MAX_TABLE_SIZE+") Deleting oldest iss location");
         }
     }
 
